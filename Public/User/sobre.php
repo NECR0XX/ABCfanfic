3e0a9184c974f47f7da34fb3b55839c83adfad64
registramos <regis11052006@gmail.com>
2024-03-20 13:25:24 -0300

    sobre

---
+++ Public/User/sobre.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nós</title>
+    <link rel="stylesheet" href="../../Resources/css/stylesobre.css">
+</head>
+<body>
+<div class="container2">
+<div class="container">
+    <div class="ret">
+    <h1>Sobre Nós</h1>
+</div>
+    <p>A Abc Fanfiction é uma organização sem fins lucrativos, criada por fãs em 2007, para servir os interesses dos fãs, fornecendo acesso e preservando a história das obras de fãs e da cultura de fãs em suas inúmeras formas. Acreditamos que as obras de fãs são transformadoras e que as obras transformadoras são legítimas.
+Somos proativos e inovadores na proteção e defesa do nosso trabalho contra a exploração comercial e desafios legais. Preservamos nossa economia, valores e expressão criativa de fãs, protegendo e nutrindo nossos colegas fãs, nosso trabalho, nossos comentários, nossa história e nossa identidade, ao mesmo tempo em que fornecemos o acesso mais amplo possível à atividade de fãs para todos os fãs.</p>
+    <p>A Abc oferece um local de hospedagem central não comercial e sem fins lucrativos para obras de fãs usando software de arquivamento de código aberto.</p>
+</div>
+</div>
+</body>
+</html>
