44082e645c314d24454944d3cb830e4a4195f9ea
registramos <regis11052006@gmail.com>
2024-04-03 11:43:53 -0300

    pinto responsivo

--- Public/User/tags.php
+++ Public/User/tags.php
@@ -6,6 +6,42 @@
     <link rel="stylesheet" href="../../Resources/css/styletag.css">
     <title>Document</title>
 </head>
+<header>
+        <div class="cabpri">
+            <div class="logo">
+                <img src="../Resources/Assets/Uploads/WhatsApp Image 2024-03-15 at 9.43.43 AM.jpeg" alt="logo" class="logo">
+                <h1>Abc fanfiction</h1>
+            </div>
+            <div class="acess">
+                <?php if(isset($_SESSION['usuarioEmail']) && isset($_SESSION['usuarioNomedeUsuario'])): ?>
+                    <button id="log" onclick="logout()">Sair</button>
+                <?php else: ?>
+                    <h3><a href="login.php">conecte-se</a></h3>
+                <?php endif; ?>
+            </div>
+        </div>
+        
+        <div class="cabsec">
+            <div class="bots">
+                <a href="User/tags.php">Tags</a>
+                <?php
+                    if(isset($_SESSION['usuarioEmail']) && isset($_SESSION['usuarioNomedeUsuario'])): ?>
+                    <a href="User/perfil.php">perfil</a><br>
+                <?php else: ?>
+                <?php endif; ?>
+                <a href="User/sobre.php">Sobre</a>
+                <a class="procurar" href="User/search.php">Procurar</a>
+
+            </div>
+            <div class="search">
+                <form id="search-form">
+                    <input type="search" id="search-input" placeholder="Pesquise Aqui">
+                        <ul id="suggestions"></ul>
+                </form>
+                    </div>
+                <script src="../Resources/Js/search.js"></script>
+            </div>
+    </header>   
 <body>
     <a href="../landing.php">Voltar</a><br><br>
     <a href="tags/fantasia.php?categoria_id=1">Fantasia</a><br>
