8878519fa79e6a82eec090036afce66a7f928d27
NECR0XX <carronananiasx@gmail.com>
2024-04-04 23:31:18 -0300

    Cu

--- Public/User/tags/comedia.php
+++ Public/User/tags/comedia.php
@@ -57,7 +57,7 @@ if (isset($_GET['categoria_id'])) {
     </header>
     <section>
     <div class="container2">
-            <h1 class= "tag2"><a href="../landing.php">Abc fanfiction</h1></a> <h1 class= "tag"> > </h1> <h1 class= "tag2"><a href="../tags.php"> Tags </h1></a> <h1 class= "tag"> > </h1> <h1 class= "tag"> Comédia </h1>
+            <h1 class= "tag2"><a href="../../landing.php">Abc fanfiction</h1></a> <h1 class= "tag"> > </h1> <h1 class= "tag2"><a href="../tags.php"> Tags </h1></a> <h1 class= "tag"> > </h1> <h1 class= "tag"> Comédia </h1>
     </div>
         <div class="fanfic-container">
             <?php
